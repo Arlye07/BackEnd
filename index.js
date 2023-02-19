864c14ef18716386502a2438804a6c90ea656e71
Arlye07 <arielvillagra7@gmail.com>
2023-02-19 18:29:27 -0300

    arreglando

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const ProductManager = require("./class");
 const producto = new ProductManager();
 
-producto.addProduct({
-title:"producto 1",
-description:"pelota",
-price:1100,
-thumbnail:"sin foto",
-code:"assdfg098",
-stock:10,
-} );
-producto.addProduct({
-    title:"producto 2",
-    description:"medias",
-    price:800,
-    thumbnail:"sin foto",
-    code:"assdfg0981",
-    stock:10,
-    } );
+// producto.addProduct({
+// title:"producto 1",
+// description:"pelota",
+// price:1100,
+// thumbnail:"sin foto",
+// code:"assdfg098",
+// stock:10,
+// } );
+// producto.addProduct({
+//     title:"producto 2",
+//     description:"medias",
+//     price:800,
+//     thumbnail:"sin foto",
+//     code:"assdfg0981",
+//     stock:10,
+//     } );
 
-    console.log(producto.getProducts());
-    producto.getProductById(2)
-    console.log(producto.getProductById(2));
+//     console.log(producto.getProducts());
+//     producto.getProductById(2)
+//     console.log(producto.getProductById(2));
 
