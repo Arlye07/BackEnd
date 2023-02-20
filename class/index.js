1a1375d4801779e98bbf3637bd208ea4334f5bb1
Arlye07 <arielvillagra7@gmail.com>
2023-02-20 12:11:00 -0300

    entregable1

--- class/index.js
+++ class/index.js
@@ -2,33 +2,44 @@ class ProductManager{
     constructor(){
         this.products =[];
     }
+    // class Product {
+    //     constructor(title, price, description, thumbnail, code, stock) {
+    //       this.title = title;
+    //       this.price = price;
+    //       this.description = description;
+    //       this.thumbnail = thumbnail;
+    //       this.code = code;
+    //       this.stock = stock;
+    //     }
+    //   }
     addProduct(product){
+
         if(this.products.find((x) => x.code === product.code)){
             console.log("Ya implementado");
             return;
 
         }
         if(
-            !product.title ||
-            !product.description ||
-            !product.price ||
-            !product.thumbnail ||
-            !product.code ||
-            !product.stock 
-            
+            !product.title || !product.description || !product.price || !product.thumbnail || !product.code ||!product.stock    
         ){
-            console.log("Faltan Datos");
+            console.log("Faltan info");
             return;
         }
         product.id = this.products.length +1;
         this.products.push(product);
-    };
+    }
+    // removeProduct(product) {
+    //     const index = this.products.indexOf(product);
+    //     if (index > -1) {
+    //       this.products.splice(index, 1);
+    //     }
+    //   }
     getProducts(){
-        return this.products ;
+        return this.products;
 
     }
     getProductsById(id){
-        const product = this.products.find((x) => product.id === id);
+        const product = this.products.find((x) => x.id === id);
         if(!product){
             console.log("No encontrado");
             return;
