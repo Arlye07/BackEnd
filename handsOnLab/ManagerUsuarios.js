97324db443197e6cd35023981efb1ca87154d73f
Arlye07 <arielvillagra7@gmail.com>
2023-02-27 23:12:55 -0300

    agregando .gitignore

--- handsOnLab/ManagerUsuarios.js
+++ handsOnLab/ManagerUsuarios.js
@@ -1,43 +1,49 @@
-const fs = require('fs')
+const fs = require("fs");
 
-class UserManager{
-    constructor(path){ 
-    this.user = []
-    this.path = this.path
+class UserManager {
+  constructor(path) {
+    this.users = [];
+    this.path = path;
+  }
+
+  async crearUsuario(usuario) {
+    try {
+      const { nombre, apellido, edad, curso } = usuario;
+
+      const userInfo = {
+        nombre,
+        apellido,
+        edad,
+        curso,
+      };
+      const existingUser = this.users.find(
+          (user) =>
+          user.nombre === userInfo.nombre && user.apellido === userInfo.apellido
+          );
+          if (!existingUser) {
+              this.users.push(userInfo);
+            }
+
+      await fs.promises.writeFile(
+        this.path,
+        JSON.stringify(this.users, null, "\t")
+      );
+
+      return "Usuario creado con exito";
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async consultarUsuarios() {
+    try {
+      const data = await fs.promises.readFile(this.path, "utf-8");
+      const response = JSON.parse(data);
+      return response;
+    } catch (error) {
+      throw(error);
+    }
+  }
 }
 
-    async crearUsuario(usuario){
-        try{
-            const{ nombre, apellido, edad, curso} = usuario
-
-        const userInfo = {
-            nombre,
-            apellido,
-            edad,
-            curso
-        }
-        this.users.push(userInfo)
-
-        await fs.promises.wriFile(this.path, JSON.stringify(this.users, null, '\t'))
-
-        return 'Usuario creado con exito'
-    } catch (error){
-        return error
-    }
-    }
-
-
-
-
-async consultarUsuarios(){
-    try{
-        const data = await fs.promises.readFile(this.path, 'utf-8')
-        const response = JSON.parse(data)
-        return response
-    } catch (error){
-        console.log(error);
-    }
-}
-}
-
-module.exports = UserManager
+module.exports = UserManager;
--- handsOnLab/users.js
+++ handsOnLab/users.js
@@ -1,6 +1,6 @@
 const UserManager = require("./ManagerUsuarios");
 
-const userManager = new UserManager("./users.json");
+const userManager = new UserManager("./Users.json");
 
 const usuario = async () =>{
     const usuario1 = {
@@ -23,10 +23,20 @@ const usuario = async () =>{
     await userManager.crearUsuario(usuario2)
     await userManager.crearUsuario(usuario3)
 
-    const response = await userManager.crearUsuario(usuario)
-    console.log(response);
-    const data = await userManager.crearUsuario()
-    console.log(data);
+
+    const usuario4 = {
+        nombre: 'Ali',
+        apellido: 'Mar',
+        curso: 2
+    }
+    const response = await userManager.crearUsuario(usuario4)
+    console.log(response)
+
+    // const response = await userManager.crearUsuario(usuario)
+    // console.log(response);
+    // const data = await userManager.crearUsuario()
+    // console.log(data);
 }
 
-usuario()
+usuario();
+
