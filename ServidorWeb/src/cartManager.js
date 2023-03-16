52babb9f7174f321eada2f3e70ffb5cedbbfd7e0
Arlye07 <arielvillagra7@gmail.com>
2023-03-16 12:33:36 -0300

    1preEntregaVillagraA

--- ServidorWeb/src/cartManager.js
+++ ServidorWeb/src/cartManager.js
@@ -1,84 +1,133 @@
-const fs = require("fs");
-const path = `${__dirname}/../data/carts.json`;
+// const fs = require('fs');
 
-class CartManager {
-  constructor(path) {
-      this.path = path;
-      //this.carts = [];
-    //this.loadCarts();
-  }
+// const express = require('express');
+// const cartRouter = require('./routes/cart.router');
 
-  creandoCart(){
-    const carrito = fs.readFileSync(this.path)
-    const carro = JSON.parse(carrito)
+// const app = express();
 
-    const id = carro.length + 1
+// app.use('/cart', cartRouter);
 
-    const addCart = {
-        "id":id,
-        "products": []
-    }
-
-    carro.push(newCart)
-    fs.writeFileSync(this.path, JSON.stringify(carro,null,2))
-    console.log($(id));
-
-  }
-
-//   addCart() {
-//     const newCart = {
-//       id: this.readFileSync(),
-//       products: []
-//     };
-//     this.carts.push(newCart);
-//     this.saveCarts();
-//     return newCart;
-//   }
-
-//   getCartById(id) {
-//     const cart = this.carts.find((x) => x.id === id);
-//     if (!cart) {
-//       throw "No encontrado";
-//     }
-//     return cart;
-//   }
-
-//   addProductToCart(cartId, productId, quantity = 1) {
-//     const cart = this.getCartById(cartId);
-//     const product = { id: productId, quantity };
-//     const index = cart.products.findIndex((p) => p.id === productId);
-//     if (index === -1) {
-//       cart.products.push(product);
-//     } else {
-//       cart.products[index].quantity += quantity;
-//     }
-//     this.saveCarts();
-//     return cart;
+// class CartManager {
+//   constructor(path) {
+//     this.path = path;
+//     this.cartData = [];
+//     this.loadCarts();
 //   }
 
 //   loadCarts() {
-//     try {
-//       const data = fs.readFileSync(this.path, "utf-8");
-//       this.carts = JSON.parse(data);
-//     } catch (error) {
-//       console.log("Error al cargar carritos:", error.message);
-//     }
+//     const cartJson = fs.readFileSync(this.path);
+//     this.cartData = JSON.parse(cartJson);
 //   }
 
 //   saveCarts() {
-//     try {
-//       fs.writeFileSync(this.path, JSON.stringify(this.carts, null, "\t"));
-//     } catch (error) {
-//       console.log("Error al guardar carritos:", error.message);
+//     fs.writeFileSync(this.path, JSON.stringify(this.cartData, null, 2));
+//   }
+
+//   createCart() {
+//     const id = this.cartData.length + 1;
+//     const cart = {
+//       id,
+//       products: []
+//     };
+//     this.cartData.push(cart);
+//     this.saveCarts();
+//     console.log(`Cart created with ID: ${id}`);
+//     return id;
+//   }
+
+//   getCart(cartId) {
+//     const cart = this.cartData.find(c => c.id === cartId);
+//     if (!cart) {
+//       console.log(`Cart ${cartId} not found`);
+//       return null;
 //     }
+//     return cart;
 //   }
 
-//   getNextId() {
-//     const ids = this.carts.map((c) => c.id);
-//     const maxId = ids.length > 0 ? Math.max(...ids) : 0;
-//     return maxId + 1;
+//   addProductToCart(cartId, productId) {
+//     const productJson = fs.readFileSync('./data/products.json');
+//     const products = JSON.parse(productJson);
+//     const product = products.find(p => p.id === productId);
+//     if (!product) {
+//       console.log(`Product ${productId} not found`);
+//       return;
+//     }
+//     const cart = this.getCart(cartId);
+//     if (!cart) {
+//       console.log(`Cart ${cartId} not found`);
+//       return;
+//     }
+//     const cartProduct = cart.products.find(p => p.id === productId);
+//     if (cartProduct) {
+//       cartProduct.quantity++;
+//     } else {
+//       cart.products.push({
+//         id: productId,
+//         quantity: 1
+//       });
+//     }
+//     this.saveCarts();
+//     console.log(`Product ${productId} added to cart ${cartId}`);
 //   }
- }
+// }
 
-const cartManager = new CartManager(path);
-module.exports = cartManager;
+// module.exports = CartManager;
+const fs = require("fs");
+
+class CartManager {
+  constructor(path, productMa){
+    this.path = path;
+    this.productManager = productMa;
+  }
+
+  addProductToCart(cartId, productId, quantity) {
+    const carts = this.getCartsArchivo();
+    const cartIndex = carts.findIndex(c => c.id === cartId);
+
+    if (cartIndex !== -1) {
+      const cart = carts[cartIndex];
+      const product = this.productManager.getProductById(productId);
+
+      if (product) {
+        const existingProductIndex = cart.products.findIndex(p => p.id === productId);
+        if (existingProductIndex !== -1) {
+          cart.products[existingProductIndex].quantity += quantity;
+        } else {
+          cart.products.push({ id: productId, quantity });
+        }
+        this.saveCartsArchivo(carts);
+      }
+    }
+  }
+
+  getCarts() {
+    return this.getCartsArchivo();
+  }
+
+  getCartsById(id) {
+    const carts = this.getCartsArchivo();
+    const cart = carts.find((c) => c.id == id);
+
+    return cart || null;
+  }
+
+  getCartsArchivo() {
+    try {
+      if (!fs.existsSync(this.path)) {
+        fs.writeFileSync(this.path, "[]");
+      }
+      const cartsData = fs.readFileSync(this.path, "utf-8");
+      const carts = JSON.parse(cartsData);
+      return carts;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  }
+
+  saveCartsArchivo(carts) {
+    fs.writeFileSync(this.path, JSON.stringify(carts));
+  }
+}
+
+module.exports = CartManager;
--- ServidorWeb/src/routes/cart.router.js
+++ ServidorWeb/src/routes/cart.router.js
@@ -1,38 +1,181 @@
-const fs = require("fs");
-const { Router } = require("express");
-const productManager = require("../productManager");
-const cartManager = require("../cartManager");
+// const fs = require('fs');
+// const { Router } = require('express');
+// const CartManager = require('../cartManager');
+// const path = require('path');
+
+// const router = Router();
+// const cartManager = new CartManager(path.resolve(__dirname, '../data/carts.json'));
+
+//-----------
+
+// // Ruta para crear un nuevo carrito
+// router.post('/', (req, res) => {
+//   const cartId = cartManager.createCart();
+//   res.json({ cartId });
+// });
+
+// // Ruta para obtener los productos de un carrito
+// router.get('/:cid', (req, res) => {
+//   const { cid } = req.params;
+//   const cart = cartManager.getCart(cid);
+//   if (cart === null) {
+//     res.status(404).json({ error: 'Carrito no encontrado' });
+//   } else {
+//     res.json(cart);
+//   }
+// });
+
+// // Ruta para agregar un producto a un carrito
+// router.post('/:cid/products/:pid', (req, res) => {
+//   const { cid, pid } = req.params;
+//   cartManager.addProductToCart(cid, pid);
+//   res.json({ message: 'Producto agregado con éxito' });
+// });
+
+// module.exports = router;
+
+///---------
+const { Router } = require('express');
+const CartManager = require('../cartManager');
+const ProductManager = require('../productManager');
 const router = Router();
-const path = require("path");
+const productMa = new ProductManager('../data/products.json');
+const cartMa = new CartManager('../data/carts.json');
 
+router.post('/', async (req, res) => {
+  try {
+    const carts = await cartMa.getCarts();
+    const cart = {
+      id: carts.length + 1,
+      products: []
+    };
+    carts.push(cart);
+    await cartMa.saveCartsArchivo(carts);
+    res.status(201).json(cart);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-// Ruta para crear un nuevo carrito
-router.post("/", (req, res) => {
-    const cartId = cartManager.createCart();
-    res.json({ cartId });
-  });
-  
-  // Ruta para obtener los productos de un carrito
-  router.get("/:cid", (req, res) => {
+router.get('/:cid', async (req, res) => {
+  try {
     const { cid } = req.params;
-    const cart = cartManager.getCartById(cid);
-    res.json(cart);
-  });
-  
-  // Ruta para agregar un producto a un carrito
-  router.post("/:cid/product/:pid", (req, res) => {
+    const cart = await cartMa.getCartsById(cid);
+    if (!cart) {
+      res.status(404).json({ error: 'Carrito No encontrado' });
+    } else {
+      res.json(cart.products);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/:cid/product/:pid', async (req, res) => {
+  try {
     const { cid, pid } = req.params;
-    const { quantity } = req.body;
-  
-    // Obtener el producto por su id
-    const product = productManager.getProductById(pid);
-  
-    // Agregar el producto al carrito
-    const cart = cartManager.getCartById(cid);
-    cart.addProduct(product, quantity);
-  
-    res.json(cart);
-  });
-  
+    const quantity = parseInt(req.body.quantity) || 1;
+    const product = await productMa.getProductById(pid);
+    if (!product) {
+      res.status(404).json({ error: 'Not found' });
+    } else {
+      const cart = await cartMa.getCartsById(cid);
+      if (!cart) {
+        res.status(404).json({ error: 'Carrito not found' });
+      } else {
+        console.log(cart, product.id, quantity);
+        await cartMa.addProductToCart(cart.id, product.id, quantity);
+        res.status(201).json({ message: 'Producto Agregado' });
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 module.exports = router;
+
+
+
+// router.post('/', async (req, res) => {
+//   try {
+//     const carts = await cartManager.getCarts();
+//     const cart = {
+//       id: carts.length + 1,
+//       products: []
+//     };
+//     carts.push(cart);
+//     await cartManager.saveCartsArchivo(carts, path.resolve(__dirname, '../data/carts.json'), fs);
+//     res.status(201).json(cart);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
+
+// router.get('/:cid', async (req, res) => {
+//   try {
+//     const { cid } = req.params;
+//     const cart = await cartManager.getCartsById(cid, path.resolve(__dirname, '../data/carts.json'), fs);
+//     if (!cart) {
+//       res.status(404).json({ error: 'Carrito No encontrado' });
+//     } else {
+//       res.json(cart.products);
+//     }
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
+
+// router.post('/:cid/product/:pid', async (req, res) => {
+//   try {
+//     const { cid, pid } = req.params;
+//     const quantity = parseInt(req.body.quantity) || 1;
+//     const product = await productManager.getProductById(pid, path.resolve(__dirname, '../data/products.json'), fs);
+//     if (!product) {
+//       res.status(404).json({ error: 'Producto no encontrado' });
+//     } else {
+//       const cart = await cartManager.getCartsById(cid, path.resolve(__dirname, '../data/carts.json'), fs);
+//       if (!cart) {
+//         res.status(404).json({ error: 'Carrito no encontrado' });
+//       } else {
+//         console.log(cart, product.id, quantity);
+//         await cartManager.addProductToCart(cart.id, product.id, quantity, path.resolve(__dirname, '../data/carts.json'), fs);
+//         res.status(201).json({ message: 'Producto Agregado con exito' });
+//       }
+//     }
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
+
+///-------
+// Ruta para crear un nuevo carrito
+// router.post("/", (req, res) => {
+//     const cartId = cartManager.createCart();
+//     res.json({ cartId });
+//   });
+  
+//   // Ruta para obtener los productos de un carrito
+//   router.get("/:cid", (req, res) => {
+//     const { cid } = req.params;
+//     const cart = cartManager.getCartById(cid);
+//     res.json(cart);
+//   });
+  
+//   // Ruta para agregar un producto a un carrito
+//   router.post("/:cid/product/:pid", (req, res) => {
+//     const { cid, pid } = req.params;
+//     const { quantity } = req.body;
+  
+//     // Obtener el producto por su id
+//     const product = productManager.getProductById(pid);
+  
+//     // Agregar el producto al carrito
+//     const cart = cartManager.getCartById(cid);
+//     cart.addProduct(product, quantity);
+  
+//     res.json(cart);
+//   });
+  
+
+// module.exports = router;
