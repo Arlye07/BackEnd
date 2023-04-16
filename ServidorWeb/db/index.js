38031a35e0987373bb3867c2fd9a913b745b3905
Arlye07 <arielvillagra7@gmail.com>
2023-04-16 20:11:13 -0300

    atrasado

--- ServidorWeb/db/index.js
+++ ServidorWeb/db/index.js
@@ -1,15 +1,17 @@
+
 const mongoose = require('mongoose')
 const {
   dbAdmin,
   dbPassword,
   dbHost,
   dbName,
-} = require('../src/config/db.config')
+} = require('../src/config/db.config') 
 
 const mongoConnect = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${dbAdmin}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority`
+      `mongodb+srv://admin:admin@ecommerce.wyifhv5.mongodb.net/ecommerce?retryWrites=true&w=majority`
+      // `mongodb+srv://${dbAdmin}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority`
     )
     console.log('db is connected')
   } catch (error) {
--- ServidorWeb/src/app.js
+++ ServidorWeb/src/app.js
@@ -4,12 +4,17 @@ const app = express();
 const {Server} = require('socket.io')
 const handlebars = require('express-handlebars')
 const mongoConnect = require('../db/index')
+//const router = require('./router/index')
 
 const productsRouter = require("./routes/products.router");
 const cartRouter = require("./routes/cart.router");
+const dbCartRouter= require("./dao/dbManager/carts.controller")
+const dbProductsRouter= require("./dao/dbManager/products.controller")
 
 app.use('/api/products', productsRouter);
 app.use('/api/cart', cartRouter);
+app.use('/api/dbCarts', dbCartRouter);
+app.use('/api/dbProducts', dbProductsRouter);
 
 
 app.use(express.json());
@@ -23,12 +28,13 @@ app.set('views',__dirname + '/views')
 app.set('view engine','handlebars')
 
 mongoConnect()
+//router(app)
 
 const httpServer = app.listen(port, () => {
    console.log(`Server listening on ${port}`);
 });
 const io = new Server(httpServer)
-const realTimeProductsRouter = require('./routes/realTimeProducts.js')
+const realTimeProductsRouter = require('./routes/realTimeProducts.js');
 app.use('/api/realTimeProducts', realTimeProductsRouter(io))
 
 io.on('connection', socket => {
